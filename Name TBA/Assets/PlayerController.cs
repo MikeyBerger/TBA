8a769d61f674b2f8af5ff7048345c4d35dc4cc59
MikeyBerger <michaeljudahberger@gmail.com>
2020-08-10 18:55:56 -0700

    Game Added

---
+++ Name TBA/Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 RotationVector;
+    private Rigidbody2D RB;
+    public float TurnSpeed;
+    public float DashSpeed;
+    private bool IsDashing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    public void OnRotation(InputAction.CallbackContext ctx)
+    {
+        RotationVector = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnDash(InputAction.CallbackContext ctx)
+    {
+        if(ctx.phase == InputActionPhase.Performed)
+        {
+            RB.AddForce(RotationVector * DashSpeed * Time.deltaTime);
+        }
+    }
+}
