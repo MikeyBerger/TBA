877ef0f156aa34fad1616da0341bd05aad5acc2e
MikeyBerger <michaeljudahberger@gmail.com>
2020-08-11 17:13:17 -0700

    Bugs Fixed

--- Name TBA/Assets/PlayerController.cs
+++ Name TBA/Assets/PlayerController.cs
@@ -9,8 +9,15 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D RB;
     public float TurnSpeed;
     public float DashSpeed;
-    private bool IsDashing;
-    public float RotZ;
+    public bool IsDashing = false;
+    public float StopDash;
+
+    IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(StopDash);
+        IsDashing = false;
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,17 +28,66 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
         //RB.AddForce(new Vector2(RotationVector.x, RotationVector.y) * DashSpeed * Time.deltaTime);
 
-        RotZ = RotationVector.x * TurnSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.forward * RotZ);
+        //RotZ = RotationVector.x * TurnSpeed * Time.deltaTime;
+        //transform.Rotate(Vector3.forward * RotZ);
         //RotationVector = new Vector3(0, 0, 0);
         //RB.MoveRotation(new Vector3(RotationVector.x, 0, RotationVector.y));
+#pragma warning disable format
+        //transform.rotation = Quaternion.LookRotation(new Vector3(RotationVector.x, 0, RotationVector.y));
+#pragma warning restore format
+
+        if (RotationVector.x == 0 && RotationVector.y == 0)
+        {
+            RotationVector = Vector2.up;
+        }
+
+
+        //Vector2 EulerVec = new Vector2(RotZ, RotX);
+        //Vector3 PlayerRot = new Vector3(RotX, 0, RotZ);
+        //Quaternion Target = Quaternion.Euler(0, 0, EulerVec);
+
+        #region Some Random Shit From the Web
+        /*
+        Quaternion PlayerRot = transform.rotation;
+        float Z = PlayerRot.z;
+        Z -= RotationVector.x * TurnSpeed * Time.deltaTime;
+
+        PlayerRot = Quaternion.Euler(0, 0, Z);
+
+        transform.rotation = PlayerRot;
+
+        Vector3 PlayerPos = transform.position;
+
+        Vector3 Velocity = new Vector3(0, RotationVector.y * TurnSpeed * Time.deltaTime, 0);
+
+        PlayerPos += PlayerRot * Velocity;
+
+        transform.position = PlayerPos;
+        */
+        #endregion
+
+        transform.up = new Vector2(RotationVector.x, RotationVector.y);
+
+        if (IsDashing)
+        {
+            RB.AddForce(RotationVector * DashSpeed * Time.deltaTime);
+            //StartCoroutine(StopDashing());
+            IsDashing = false;
+        }
+
+        if (!IsDashing)
+        {
+            transform.position = Vector2.zero;
+        }
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, PlayerRot * Angle, Time.deltaTime * TurnSpeed);
     }
 
     public void OnRotation(InputAction.CallbackContext ctx)
@@ -41,9 +97,9 @@ public class PlayerController : MonoBehaviour
 
     public void OnDash(InputAction.CallbackContext ctx)
     {
-        if(ctx.phase == InputActionPhase.Performed)
+        if (ctx.phase == InputActionPhase.Performed)
         {
-            RB.AddForce(RotationVector * DashSpeed * Time.deltaTime);
+            IsDashing = true;
         }
     }
 }
